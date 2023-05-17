eb6fb76c6844cb8ee22774d67833d9b37501ded6
heojun0827 <heojun0827.github.io>
2023-05-17 18:08:49 +0900

    pro03 start

---
+++ src/kr/go/yeosu/test/MysqlTest.java
@@ -0,0 +1,14 @@
+package kr.go.yeosu.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MysqlTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
